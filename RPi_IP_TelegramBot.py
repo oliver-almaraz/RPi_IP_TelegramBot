a0b81056abea8f76d25375f88264bf7d03d4e545
Oliver Almaraz <oliver.almaraz@gmail.com>
2020-12-16 17:20:24 -0600

    Create RPi_IP_TelegramBot.py

---
+++ RPi_IP_TelegramBot.py
@@ -0,0 +1,15 @@
+import socket
+import telegram_send
+
+## Dependencies:
+# pip install telegram_send
+# telegram-send --configure
+
+# If pip is not installed:
+# sudo apt install python-pip (or for Arch based distros: sudo pacman -Syyuu python pip)
+# echo 'PATH="$HOME/.local/bin:$PATH"' >> ~/.profile
+# . ~/.profile
+
+host_name = socket.gethostname()
+IP_addres = socket.gethostbyname(h_name)
+telegram_send.send(messages=[f"IP adress for '{host_name}' is:\n{IP_addres}"])
